f694eb9027b7b5959a5c2ef9be36879413808b61
Sujith M A <sujithma@taxbackinternational.com>
2019-09-03 13:15:46 +0530

    internatiolisation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internatiolisation-demo';
+  languageList = [ // <--- add this
+    { code: 'http://localhost:4201/en', label: 'English' },
+    { code: 'http://localhost:4200/es', label: 'Spanish' }
+  ];
 }
